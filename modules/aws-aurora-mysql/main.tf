fb38004dab3aa21d9aad15556d8d9e6a4bcc1b8d
nida.bandukwala <nida.bandukwala@ibm.com>
2026-03-09 11:21:29 -0400

    Revert local change
    Signed-off-by: nida.bandukwala <nida.bandukwala@ibm.com>

--- modules/aws-aurora-mysql/main.tf
+++ modules/aws-aurora-mysql/main.tf
@@ -18,7 +18,7 @@ module "common_aws-configuration" {
 # Use the dedicated Aurora MySQL CloudWatch registration module
 module "common_aurora-mysql-cloudwatch-registration" {
   count  = var.log_export_type == "Cloudwatch" ? 1 : 0
-  source = "/Users/nida/GuardiumInsights/Terraform/terraform-guardium-common/modules/aurora-mysql-cloudwatch-registration"
+  source = "IBM/common/guardium//modules/aurora-mysql-cloudwatch-registration"
 
   aws_region                = var.aws_region
   aws_account_id            = local.aws_account_id
