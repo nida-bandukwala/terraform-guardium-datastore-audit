759ea99689d42aa4a7dfbd164b44fc1b3d275c30
nida.bandukwala <nida.bandukwala@ibm.com>
2026-03-11 15:15:56 -0400

    Add param group
    Signed-off-by: nida.bandukwala <nida.bandukwala@ibm.com>

--- examples/aws-aurora-mysql-audit/variables.tf
+++ examples/aws-aurora-mysql-audit/variables.tf
@@ -29,6 +29,30 @@ variable "aurora_mysql_cluster_identifier" {
   default     = "guardium-aurora-mysql"
 }
 
+variable "force_failover" {
+  type        = bool
+  default     = false
+  description = "To failover the database cluster, requires multi AZ databases. Results in minimal downtime"
+}
+
+variable "audit_events" {
+  type        = string
+  description = "Comma-separated list of events to audit (CONNECT,QUERY,TABLE,QUERY_DDL,QUERY_DML,QUERY_DCL)"
+  default     = "CONNECT,QUERY"
+}
+
+variable "audit_incl_users" {
+  type        = string
+  description = "Comma-separated list of users to include in audit logs (server_audit_incl_users). If set, only these users will be audited. Leave empty to audit all users."
+  default     = ""
+}
+
+variable "audit_excl_users" {
+  type        = string
+  description = "Comma-separated list of users to exclude from audit logs (server_audit_excl_users). Example: 'rdsadmin,testuser'. The rdsadmin user queries the database every second for health checks, which can cause log files to grow quickly."
+  default     = "rdsadmin"
+}
+
 variable "cloudwatch_logs_exports" {
   type        = list(string)
   description = "List of log types to export to CloudWatch. Valid values for Aurora MySQL: audit, error, general, slowquery"
@@ -148,12 +172,12 @@ variable "use_aws_bundled_ca" {
 
 variable "prefix" {
   type        = bool
-  description = "Whether to use the AWS bundled CA certificates for CloudWatch connection"
+  description = "This is the prefix."
   default     = false
 }
 
 variable "unmask" {
   type        = bool
-  description = "Whether to use the AWS bundled CA certificates for CloudWatch connection"
+  description = "This is the unmask value"
   default     = false
 }
